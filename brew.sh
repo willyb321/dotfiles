3025f637634dabd76861f712ac0f35d1bd72637e
William Blythe <wbwilliam7@gmail.com>
2016-10-18 17:05:30 +1100

    update brew.sh, sublime, and .macos

--- brew.sh
+++ brew.sh
@@ -18,88 +18,173 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
 
 # Install more recent versions of some macOS tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+#brew install aircrack-ng
+#brew install bfg
+#brew install binutils
+#brew install binwalk
+#brew install cifer
+#brew install dex2jar
+#brew install dns2tcp
+#brew install fcrackzip
+#brew install foremost
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
+#brew install nmap
+#brew install pngcheck
+#brew install socat
+#brew install sqlmap
+#brew install tcpflow
+#brew install tcpreplay
+#brew install tcptrace
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install xpdf
+#brew install xz
 
 # Install other useful binaries.
-brew install ack
-brew install dark-mode
+#brew install ack
+#brew install dark-mode
 #brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew install vbindiff
-brew install webkit2png
-brew install zopfli
+#brew install git
+#brew install git-lfs
+#brew install imagemagick --with-webp
+#brew install lua
+#brew install lynx
+#brew install p7zip
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install rhino
+#brew install speedtest_cli
+#brew install ssh-copy-id
+#brew install testssl
+#brew install tree
+#brew install vbindiff
+#brew install webkit2png
+#brew install zopfli
+brew install antibody \
+archey \
+autoconf \
+automake \
+axel \
+boost \
+cairo \
+cmake \
+doxygen \
+ffmpeg \
+fftw \
+fontconfig \
+freetype \
+fribidi \
+gd \
+gdbm \
+gdk-pixbuf \
+geoip \
+gettext \
+giflib \
+giflossy \
+git \
+glib \
+gmp \
+gnutls \
+go \
+gobject-introspection \
+graphicsmagick \
+harfbuzz \
+highlight \
+hiredis \
+httrack \
+icu4c \
+imagemagick \
+intltool \
+jasper \
+jpeg \
+json-c \
+json-glib \
+lame \
+libass \
+libcroco \
+libdnet \
+libexif \
+libffi \
+libgpg-error \
+libgphoto2 \
+libgsf \
+libicns \
+libksba \
+libpng \
+librsvg \
+libtasn1 \
+libtiff \
+libtool \
+libusb \
+libusb-compat \
+libyaml \
+libzip \
+little-cms2 \
+lua \
+luajit \
+makedepend \
+mobile-shell \
+mysql \
+net-snmp \
+nettle \
+node \
+ntopng \
+openjpeg \
+openssl \
+orc \
+pango \
+pcre \
+pixman \
+pkg-config \
+poppler \
+postgresql \
+protobuf \
+py2cairo \
+pyenv \
+pyenv-virtualenv \
+pygobject3 \
+python \
+python3 \
+readline \
+reaver \
+redis \
+rrdtool \
+ruby \
+sane-backends \
+shared-mime-info \
+speedtest_cli \
+sphinx-doc \
+sqlite \
+texi2html \
+thefuck \
+tldr \
+tree \
+vips \
+webp \
+wget \
+wine \
+x264 \
+xvid \
+xz \
+yarn \
+yasm \
+youtube-dl \
+z \
+zeromq
 
 # Remove outdated versions from the cellar.
 brew cleanup
